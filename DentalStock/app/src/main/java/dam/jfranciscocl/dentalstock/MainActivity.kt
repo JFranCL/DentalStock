d74457abd2eb14648178fe34f21e1ea10df98741
JFranCL <contreras.lopez.j.francisco@gmail.com>
2021-02-16 20:19:13 +0100

    Project Creation

---
+++ DentalStock/app/src/main/java/dam/jfranciscocl/dentalstock/MainActivity.kt
@@ -0,0 +1,11 @@
+package dam.jfranciscocl.dentalstock
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
